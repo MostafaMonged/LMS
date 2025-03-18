1fd7aa8375d0b500f470b5eb1eaed15c7ff2c405
MostafaMonged <mongedmostafa19@gmail.com>
2025-03-18 17:16:57 +0200

    removed 2 models

--- app/models/Notification.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from app import db
-
-class Notification(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    message = db.Column(db.String(255), nullable=False)
-    is_read = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
